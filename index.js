81c1679188e4f4092fdb8f3210c07da49b397861
Sigadong <irwanto.regar30@gmail.com>
2022-07-31 12:21:49 +0700

    regular-expression pada routing

--- index.js
+++ index.js
@@ -2,35 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// #ExpressJS menyediakan mekanisme yang mudah untuk menangani kasus parameter id yaitu dengan routing dinamis.
-app.get('/post/:id', (req, res) => {
-  res.send(`Article-${req.params.id}`)
+// # Regular Expression Pada Routing
+
+/* Salah satu kelebihan dari routing di ExpressJS adalah mendukung regular expression (regex) pada route.
+Misalnya expresi * digunakan untuk merepresentasikan karakter apapun. 
+Contoh: */
+app.get('/page-*', (req, res) => {
+  res.send('Route: ' + req.path);
 });
 
-/* Cara lain untuk membuat dinamis routing adalah dengan menggunakan query string pada URL.
-  Contoh: /foods
-          /foods?page=2
-          /foods?page=2&sort=title
+/* 
+Routing di atas akan cocok untuk semua routing dengan method GET yang diawali dengan path /page-.
+Jadi misalnya ketika ada requuest /page-apapun maka akan menampilkan respon: route: /page-apapun
 
-Yaitu dengan menambahkan tanda tanya ? di depan nama parameter, dan untuk mendefinisikan nilai dari
-parameter dengan menggunakan tanda sama dengan = diikuti nilai dari parameternya.
-Jika parameternya lebih dari satu maka dipisahkan dengan tanda &
+Contoh lain:
+- /xy?z akan match dengan /xyz & /xz (tanda tanya artinya karakter sebelumnya boleh ada boleh tidak atau opsional).
+- Jika karakter opsional lebih dari satu maka gunakan tanda kurung '/food(s)?' maka akan match dengan routing /food dan /foods
+- Kebalikan karakter opsional adalah karakter yang menunjukkan harus ada minimal satu yaitu karakter +, maka routing '/page(s)+' akan match dengan URL /pages, /pagess, dsb (karakter s setelah page minimal satu)
 */
-app.get('/foods', (req, res) => {
-  console.log(req.query);
-  res.end();
+app.get('/xy?z', (req, res) => {
+  res.send('Route: ' + req.path);
+});
+app.get('/food(s)?', (req, res) => {
+  res.send('Route: ' + req.path);
+});
+app.get('/page(s)+', (req, res) => {
+  res.send('Route: ' + req.path);
 });
 
-/* Apabila kita akses dengan URL http://localhost:3000/foods?page=2&sort=title, maka pada log akan muncul: { page: '2', sort: 'title' }
-  Yaitu objek yang merupakan parameter query string yang dikirimkan melalui URL. 
-  Dengan pemahaman ini maka kita bisa bereksperimen untuk melakukan sesuatu berdasarkan nilai dari parameter pada query string tersebut
-*/
-app.get('/foodst', (req, res) => {
-  const page = req.query.page ? req.query.page : 1
-  res.write('Foods page: ' + page + '\n');
-  if (req.query.sort) res.write('Sort by: ' + req.query.sort);
-  console.log(req.query);
-  res.end();
-});
+// Dengan menggunakan konsep regex tersebut maka dapat digabung dengan konsep dinamis routing, misalnya:
+app.get('/post/:id?', (req, res) => {
+  res.send('artikel-' + req.params.id)
+})
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
