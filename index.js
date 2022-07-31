3648a107fb8481fb37a03303d37ff03d5703e0d5
Sigadong <irwanto.regar30@gmail.com>
2022-07-31 11:40:31 +0700

    routing dinamis

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// #Mengirim berbagai jenis response, di mana argumennya bisa berisi konten serta mengakhiri proses response.
-app.get('/', (req, res) => res.send('Hello World!'));
 
-// #res.write()=>menulis konten ke response. res.end()=>mengakhiri proses response di mana argumennya bisa berisi konten.
-app.get('/hello', (req, res) => {
-  res.write('Hello ')
-  res.write('World!')
-  res.end()
+// #ExpressJS menyediakan mekanisme yang mudah untuk menangani kasus parameter id yaitu dengan routing dinamis.
+app.get('/post/:id', (req, res) => {
+  res.send(`Article-${req.params.id}`)
 });
 
-// HTTP method lainnya
-app.post('/contoh', (req, res) => res.send('Request dengan method POST'));
-app.put('/contoh', (req, res) => res.send('Request dengan method PUT'));
-app.delete('/contoh', (req, res) => res.send('Request dengan method DELETE'));
+/* Cara lain untuk membuat dinamis routing adalah dengan menggunakan query string pada URL.
+  Contoh: /foods
+          /foods?page=2
+          /foods?page=2&sort=title
 
-// #Selain itu kita juga bisa mendefinisikan route yang sama untuk semua method yaitu dengan mengubahnya menjadi all()
-app.all('/universal', function (req, res) {
-  res.send('request dengan method ' + req.method)
+Yaitu dengan menambahkan tanda tanya ? di depan nama parameter, dan untuk mendefinisikan nilai dari
+parameter dengan menggunakan tanda sama dengan = diikuti nilai dari parameternya.
+Jika parameternya lebih dari satu maka dipisahkan dengan tanda &
+*/
+app.get('/foods', (req, res) => {
+  console.log(req.query);
+  res.end();
+});
+
+/* Apabila kita akses dengan URL http://localhost:3000/foods?page=2&sort=title, maka pada log akan muncul: { page: '2', sort: 'title' }
+  Yaitu objek yang merupakan parameter query string yang dikirimkan melalui URL. 
+  Dengan pemahaman ini maka kita bisa bereksperimen untuk melakukan sesuatu berdasarkan nilai dari parameter pada query string tersebut
+*/
+app.get('/foodst', (req, res) => {
+  const page = req.query.page ? req.query.page : 1
+  res.write('Foods page: ' + page + '\n');
+  if (req.query.sort) res.write('Sort by: ' + req.query.sort);
+  console.log(req.query);
+  res.end();
 });
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
