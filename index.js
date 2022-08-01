59239e14a053c5d53ff0c0ff4419c3bb493ba86c
Sigadong <irwanto.regar30@gmail.com>
2022-08-01 19:23:00 +0700

    middleware expressjs | menangani file upload

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = 3000;
 const routers = require('./routers');
 app.use(routers);
 
-const path = require('path')
-// middleware static
-app.use('/public', express.static(path.join(__dirname, 'public')))
+
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
--- routers.js
+++ routers.js
@@ -3,81 +3,108 @@ const routers = express.Router();
 
 routers.get('/', (req, res) => res.send('Hello World!'));
 
-routers.get('/post/:id', (req, res) => {
-  if (req.params.id) res.send('Article' + req.params.id);
-});
+/* ## Menangani File Upload
+ Belajar menangani file yang diupload oleh user. Bagaimana menangkap file tersebut dari form yang dikirimkan oleh user dan kemudian menyimpannya ke suatu direktori tertentu.
+Menangani file upload maka kita bisa menggunakan "Multer" merupakan middleware untuk menangani file upload menggunakan multipart/form-data. Multer sendiri dibangun diatas pustaka busboy.
 
-/* 
-Response dari ExpressJS tidak hanya berupa data berformat string, namun juga bisa berupa file yang
-ada di server. Salah satu method yang bisa kita gunakan adalah res.sendFile()
+Kemudian definisikan direktori yang menjadi target dari file upload melalui argumen dest pada method multer. Misalnya target upload adalah direktori 'public'.
+
+Method single pada variabel upload digunakan jika file yang diupload hanya satu file.
+Argumen pada method single merupakan nama dari field 'file' pada form upload nantinya, artinya kita bisa ganti sesuai
+kebutuhan kita, misal: image, photo, cover, dokumen, dll.
+Jika kita ingin mengupload banyak file sekaligus maka kita bisa gunakan method array: 
+  => upload.array('files').
 */
-// routers.get('/download', function (req, res) {
-//   const filename = 'ubd.png';
-//   res.sendFile(__dirname + '/' + filename);
-// });
-/* Perintah __dirname akan mengembalikan direktori dari current file atau dalam hal ini routers.js. Hal ini
-dilakukan sebab file yang ingin kita tampilkan (logo.png) berada pada lokasi yang sama dengan
-routers.js.
-String / digunakan untuk menggabungkan direktori dengan nama filenya. Sebenarnya untuk lebih
-amannya, kita bisa gunakan pustaka path untuk menggabungkan direktori dengan nama filenya. */
-// require dulu
+// middleware multer
+const multer = require('multer');
+// const upload = multer({ dest: 'public' });
+
+// Validasi melalui argumen fileFilter pada muller
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, false);
+  }
+  cb(null, true);
+}
+const upload = multer({ dest: 'public', fileFilter: imageFilter });
+
+
+routers.post('/upload', upload.single('file'), (req, res) => {
+  res.send(req.file);
+});
+// Nah yang menarik adalah nama file yang diupload berbeda dengan nama asilnya yaitu awalnya logo.jpg menjadi ce5f25767fb8333934443fa397142d9d, yaitu otomatis akan dihash.
+
 const path = require('path')
 
-routers.get('/download', function (req, res) {
-  const filename = 'logo.png';
-  // res.sendFile(__dirname + '/' + filename);
-  // implementasinya
-  res.sendFile(path.join(__dirname, filename));
+/* Untuk mengubah nama filenya menjadi nama lain, maka kita bisa gunakan pustaka fs (bawaan NodeJS) melalui method renameSync.
+    => fs.renameSync(oldPath, newPath)    */
+const fs = require('fs');
+routers.post('/upload2', upload.single('file'), (req, res) => {
+  const file = req.file;
+  if (file) {
+    const target = path.join(__dirname, 'public', file.originalname);
+    fs.renameSync(file.path, target);
+    res.send('file berhasil diupload');
+  } else {
+    res.send('file gagal diupload');
+  }
 });
 
-/* 
-Ketika sebuah file terdownload maka otomatis nama file yang terdonwload akan sama dengan nama file
-asli di server atau dalam hal ini logo.png.
-Nah, untuk alasan tertentu terkadang kita perlu mengubah nama file yang terdownload denga nama yang
-berbeda dengan nama aslinya. Maka kita bisa melakukannya dengan menyisipkan header ContentDisposition sebagai argumen kedua ketika menjalankan method sendFile.
-Cara ini juga berlaku untuk file jenis apapun yang ingin disediakan untuk didownload.
+/* 1.Bagaimana memfilter misalnya hanya file image saja yang bisa diupload ?
+  Validasi ini dimungkinan melalui argumen 'fileFilter' pada muller.
+    code: const imageFilter = (req, file, cb) => {...}
+          const upload = multer({ dest: 'public', fileFilter: imageFilter });
+  Dengan cara ini maka apabila file yang diupload bukan image maka file tersebut tidak akan diupload oleh aplikasi
 */
-// routers.get('/download2', function (req, res) {
-//   const filename = 'logo.png';
-//   res.sendFile(path.join(__dirname, filename), {
-//     headers: {
-//       'Content-Disposition': 'attachment; filename="logo-23bk3kh4lk29lll28.png"'
-//     }
-//   });
-// });
-
-
-/* 
-sebenarnya kita bisa juga membuat file gambar itu tampil pada browser alias tidak langsung
-terdownload apalagi image merupakan file yang didukung oleh browser.
-Untuk melakukannya maka kita bisa sertakan jenis konten (Content-Type) dari file image tersebut
-(image/png) yang kita berikan pada argumen kedua method sendFile.
-*/
-routers.get('/preview-image', function (req, res) {
-  const filename = 'logo.png';
-  res.sendFile(path.join(__dirname, filename), {
-    headers: {
-      'Content-Type': 'image/png'
-    }
-  });
+routers.post('/upload3', upload.single('file'), (req, res) => {
+  req.file ? res.send('file filter berhasil diupload') :
+    res.send('file gagal diupload')
 });
-// Menampilkan file dengan metode ini hanya cocok untuk file-file yang sifatnya private atau hanya boleh diakses oleh user tertentu saja
 
 
-/* 
-Jika file yang dimaksud bersifat public maka cara ini sangat tidak disarankan karena akan membebani
-server aplikasi kita.
-Untuk itulah kita bisa menggunakan built-in middleware yaitu "static". Sebagai contoh kita punya sebuah
-direktori bernama public sebagai tempat dimana kita meletakkan file-file asset dari web kita.
-  code: app.use(express.static('public'))
-Catatan: Lokasi direktori public adalah relatif alias sejajar dengan file aplikasi. Lebih bagus lagi jika kita deklarasikan direktori absolutenya:
-  code: app.use(express.static(path.join(__dirname, 'public')))
-
-Dengan cara ini maka kita bisa mengakses file-file dalam folder public secara langsung. Misalnya jika di
-dalam folder tersebut terdapat file contoh.pdf maka kita bisa mengaksesnya melalui URL http://localhost:3000/contoh.pdf, maka file PDF akan terbuka atau terdownload jika tidak ada plugin PDF pada browser.
-Selain itu kita juga mendefinisikan routing sebagai prefik ketika mengakses file tersebut, misalnya:
-  app.use('/public', express.static(path.join(__dirname, 'public')))
-Maka sekarang untuk mengakses file contoh.pdf melalui URL http://localhost:3000/public/contoh.pdf.lL
+/* 2.Bagaimana jika form tidak hanya berisi file upload saja, misalnya kita ingin mengupload dua field
+dimana field pertama berupa string sedangkan field kedua berupa file?
+Caranya, mudah saja, dimana untuk menangkap field pertama kita bisa gunakan metode biasa yaitu
+req.body.NAMA_FIELD sedangkan untuk menangkap field file kita gunakan cara sebelumnya yaitu req.file
 */
+routers.post('/register', upload.single('avatar'), (req, res) => {
+  const name = req.body.name
+  const avatar = req.file
+  res.send({ name: name, avatar: avatar });
+});
+
+
+/* 3.Bagaimana jika ada banyak file yang diupload sekaligus?
+Caranya dengan menggunakan method array sebagai pengganti single. Formatnya adalah: upload.array(NAMA_FIELD, JUMLAH_MAX_FILE).
+    routers.post('/photos/upload', upload.array('photos', 12), (req, res) => {
+      // req.files is array of `photos` files
+    });
+Gunakan req.files untuk menangkap file photos, dimana req.files berupa array dari file photos.
+*/
+
+
+/* 4.Bagaimana jika ada beberapa field bertipe file dalam satu form upload?
+Multer juga memiliki fitur untuk menangani kasus ini yaitu menggunakan upload.fields.
+Sebagai contoh pada form terdapat dua field bertipe file yaitu avatar dan gallery.
+*/
+var cpUpload = upload.fields([
+  { name: 'avatar', maxCount: 1 },
+  { name: 'gallery', maxCount: 4 }
+]);
+
+routers.post('/profile', cpUpload, function (req, res, next) {
+  const avatar = req.files['avatar'][0]; //-> File
+  const gallery = req.files['gallery']; //-> Array
+  res.send({ avatar, gallery });
+});
+
+
+/* 5.Bagaimana jika filenya hanya bertipe teks saja?
+  Maka gunakan multer().none(). */
+routers.post('/login', multer().none(), function (req, res, next) {
+  const name = req.body.name;
+  res.send({ name });
+});
+
 
 module.exports = routers;
