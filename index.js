983dc06467a7f91829c3a06165d5ca6984248b63
Sigadong <irwanto.regar30@gmail.com>
2022-08-01 11:51:01 +0700

    middleware expressjs | menangani request body

--- index.js
+++ index.js
@@ -3,64 +3,35 @@ const app = express();
 const port = 3000;
 
 // # Middleware ExpressJS
-/* 
-Kata middleware dari sisi istilah berarti perangkat yang berada ditengah tengah.
-Middleware bertindak sebagai penengah antara client request dengan pemrosesan request.
-Middleware bekerja melakukan intercept pada sistem routing.
-Secara umum, middleware digunakan untuk menjalankan fungsi-fungsi tertentu ketika routing tertentu
-diakses.
-
-Middleware berupa sebuah fungsi, dimana fungsi ini memiliki tiga argumen yaitu req (objek request), res
-(objek response) dan next (fungsi).
-*/
-/* 
-## Menangani Log
-Misalnya kita akan membuat middleware yang berfungsi menampilkan log pada tiap request terhadap
-route apapun.
-Adapun data log yang ingin ditampilkan adalah:
-- data waktu saat request dilakukan Date.now() (timestamp),
-- ip address client req.ip dan
-- route yang diakses req.originalUrl.  */
-
-// middleware log
-const log = (req, res, next) => {
-  console.log(`Date: ${Date.now()} | IP: ${req.ip} | Url: ${req.originalUrl}`)
-  next();
-}
-app.use(log);
 
 // deklarasi routing
 const routers = require('./routers');
 app.use(routers);
+/* 
+Request dari client terkadang juga menyertakan konten melalui body, terutama request dari form, atau
+terkadang request yang dikirimkan beformat JSON (application/json).
+Untuk menangani request tersebut, kita bisa menggunakan middleware bawaan ExpressJS yaitu bodyparser.
+Middleware ini digunakan untuk memparsing object request tepatnya pada body baik yang diencode
+(application/x-www-form-url-encode) maupun yang berformat json dari request client sehingga menjadi
+mudah dibaca.
+*/
+// const bodyParser = require('body-parser');
 
-// middleware menangani 404
-const notFound = (req, res, next) => {
-  res.json({
-    status: 'error',
-    message: 'resource tidak ditemukan',
-  });
-}
-app.use(notFound);
+// Penggunaan middleware body parser //parse x-www-form-url-encode
+// app.use(bodyParser.urlencoded({ extended: true }));
+// parse JSON
+// app.use(bodyParser.json());
 
-/*  
-## Menangani Error
-Contoh lain lagi, kita juga bisa menangani error dengan menggunakan middleware, namun argumennya
-sedikit berbeda, terdapat satu argumen tambahan yaitu err yang merepresentasikan objek error.
 
-const errorHandling = (err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Terjadi kesalahan')
-  }
-  app.use(errorHandling)
 
- Implementasinya, middleware ini akan menangani semua jenis error yang mungkin timbul sehingga responnya tetap dapat kita kontrol.
- Supaya lebih web service friendly maka kita bisa ubah menjadi format json. */
-const errorHandling = (err, req, res, next) => {
-  res.json({
-    status: 'error',
-    message: 'terjadi kesalahan pada server',
-  });
-}
-app.use(errorHandling);
+/* Sejak ExpressJS versi 4.16+, fungsi middleware body-parser ini sudah built-in di dalam
+express, sehingga kita tidak perlu lagi menggunakan middleware tersebut. */
+// parse x-www-form-url-encode
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+// parse JSON
+// app.use(bodyParser.json())
+app.use(express.json())
+
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
--- routers.js
+++ routers.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const routers = express.Router()
+const routers = express.Router();
 
 routers.get('/', (req, res) => res.send('Hello World!'));
 
 routers.get('/post/:id', (req, res) => {
   if (req.params.id) res.send('Article' + req.params.id);
 });
+
+/* 
+Sebagai contoh, kita akan membuat routing login dengan method post, dimana routing ini menerima
+request berupa data username dan password yang dikirim via form login.
+Karena kita telah memasang middleware "body-parser" maka kita bisa mendapatkan data form
+login yang dikirim oleh cient dengan cara yang cukup mudah
+*/
+routers.post('/login', (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  res.send(`Anda login dengan username dan password
+  `);
+});
+
 
 module.exports = routers;
