f46cef3face0e5841edad29dd00101c555f6ca51
Sigadong <irwanto.regar30@gmail.com>
2022-08-05 19:42:10 +0700

    menampilkan data collection

--- mongodb.js
+++ mongodb.js
@@ -1,27 +1,28 @@
 const MongoClient = require("mongodb").MongoClient;
 const connectionString = "mongodb://localhost:27017";
 
-// Melakukan koneksi dengan fungsi callback pada method connect.
-MongoClient.connect(connectionString, { useUnifiedTopology: true },
-  (error, client) => {
-    if (error) return console.error(error);
-    console.log("Server database connect!");
-  });
-
-
-// Melakukan koneksi dengan metode Promise.
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log("Server database connect!");
-  })
-  .catch(error => console.error(error));
-
-
-// Melakukan koneksi dengan metode async await.
 (async () => {
   try {
     const client = await MongoClient.connect(connectionString, { useUnifiedTopology: true });
-    console.log("Server database connect!");
+    const db = client.db('latihan');
+
+    // Kode query ke collection quotes 
+    // Untuk menampilkan seluruh data dokumen yang ada pada collecion
+    const quotes = await db.collection('quotes').find().toArray();
+
+    // Untuk menampilkan satu data dokumen quotes
+    const quote = await db.collection('quotes').findOne();
+
+    // Untuk menampilkan data dengan kriteria tertentu
+    const quoteOne = await db.collection('quotes').find({ word: 'Gitu aja kok repot' }).toArray();
+
+    // Untuk menampilkan data quotes yang ada kata
+    const quoteLike = await db.collection('quotes').find({ word: /Gitu/ }).toArray();
+
+    console.log(quotes);
+    console.log(quote);
+    console.log(quoteOne);
+    console.log(quoteLike);
   } catch (error) {
     console.error(error);
   }
