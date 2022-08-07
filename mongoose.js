98fa84615683e743551d4ee34de58a7ea8b54b35
Sigadong <irwanto.regar30@gmail.com>
2022-08-07 22:01:03 +0700

    Object Document Mapping | getting started mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
+// membuat koneksi ke mongoDB
 mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true, });
 
+// membuat schema untuk collection quotes
+const quoteSchema = new mongoose.Schema({
+  word: String,
+});
+
+// membuat model untuk schema quote
+const Quote = mongoose.model('Quote', quoteSchema);
+
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => console.log('Server database connect!'));
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', () => {
+  // membuat quote baru
+  const quote = new Quote({ word: 'Besar pasak dari pada tiang' });
+
+  // menyimpan quote
+  quote.save((error, quote) => {
+    if (error) return console.log(error);
+    console.log(quote);
+  });
+});
