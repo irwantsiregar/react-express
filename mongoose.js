dd25ad459442f34091259f12b762a7086ee83b5a
Sigadong <irwanto.regar30@gmail.com>
2022-08-07 21:46:25 +0700

    Object Document Mapping | mongoose->database connect

---
+++ mongoose.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true, });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => console.log('Server database connect!'));
