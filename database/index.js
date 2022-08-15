d31fe546f55ee7038daa509ad1b3fc427d284bd9
Sigadong <irwanto.regar30@gmail.com>
2022-08-15 20:21:10 +0700

    create product entity: CRUD Product

---
+++ database/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const { dbHost, dbName, dbPort, dbUser, dbPass } = require('../app/config');
+mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+module.exports = db;
--- routers.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const routers = express.Router();
-
-routers.get('/', (req, res) => res.send('Hello World!'));
-
-module.exports = routers;
