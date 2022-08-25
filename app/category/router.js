7ffbfbe5d3a4bf2248fe799847f0849d2b3329bf
Sigadong <irwanto.regar30@gmail.com>
2022-08-25 16:13:50 +0700

    first commit

--- app/category/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const multer = require('multer');
-const categoriesController = require('./controller');
-
-router.post('/categories', multer().none(), categoriesController.store);
-router.put('/categories/:id', multer().none(), categoriesController.update);
-router.delete('/categories/:id', categoriesController.destroy);
-
-module.exports = router;
--- app/invoice/router.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller');
-
-router.get('/invoices/:order_id', controller.show);
-
-module.exports = router;
