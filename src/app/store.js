73760bd9bbd6057794792619bf730f0a93769170
Sigadong <irwanto.regar30@gmail.com>
2022-09-01 22:03:16 +0700

    create redux state products and view list product

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import authReducer from '../features/Auth/reducer';
+import productReducer from '../features/Products/reducer';
 import thunk from 'redux-thunk';
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducers = combineReducers({ auth: authReducer });
+const rootReducers = combineReducers({ auth: authReducer, products: productReducer, });
 
 const store = createStore(rootReducers, composerEnhancer(applyMiddleware(thunk)));
 
